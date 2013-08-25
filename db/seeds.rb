6daa2b4e1da71a8162eea2c518b5d6590b4fc75c
sdmgithub <sorrows@mail.ru>
2013-08-25 21:55:35 +1100

    ADD tags.csv with list of tags; REF seed.rb code;

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,5 @@
 require 'csv'
 Tag.delete_all
 ActiveRecord::Base.connection.reset_pk_sequence!('tags')
-CSV.foreach('db/data/tags.csv', headers: true) do |row_item|
-  tag = Tag.create! row_item.to_hash
-end
+CSV.foreach('db/data/tags.csv', headers: true) { |row_item| Tag.create! row_item.to_hash }
 
